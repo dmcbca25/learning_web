9165043f3ecc39b376bfe2ab198456fd1b854417
dmcbca25 <dmcbca25@gmail.com>
2025-11-16 09:07:32 +0545

    created-dbconnection-passing-form-data-to-mysql-db-table

--- action.php
+++ action.php
@@ -1,3 +1,21 @@
 <?php   
-echo "<pre>";
-print_r($_POST);
+
+    include 'db.php';
+
+   if (isset($_POST['btn_contact'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+
+        $stmt = $conn->prepare("INSERT INTO contactForm (name, email, message) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $name, $email, $message);
+
+        if ($stmt->execute()) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
